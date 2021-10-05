ddcb62922ed7f6d5b944fdf5a91f3eec996d35c2
Ramez Al-Rabadi <54866373+ramrabadi@users.noreply.github.com>
2021-10-05 15:45:46 -0400

    Update text color to white

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -6,7 +6,7 @@ export default function AboutComponent() {
     <div class="hero min-h-screen bg-base-200 overflow-auto">
       <div class="text-left hero-content">
         <div class="max-w-lg">
-          <h1 class="mb-8 text-6xl font-bold underline">About</h1>
+          <h1 class="text-white mb-8 text-6xl font-bold underline">About</h1>
           <div data-tip="😃" class="tooltip">
             <a
               href="https://github.com/ramrabadi/ramrabadi.github.io/raw/main/Documents/Resume.pdf"
@@ -17,32 +17,36 @@ export default function AboutComponent() {
               Download My Resume!
             </a>
           </div>
-          <p class="font-mono mb-8 text-lg">👋🏽 Hi, my name is Ramez.</p>
-          <p class="font-mono mb-8 text-lg">
+          <p class="text-white font-mono mb-8 text-lg">
+            👋🏽 Hi, my name is Ramez.
+          </p>
+          <p class="text-white font-mono mb-8 text-lg">
             🎓 I recently graduated with a Bachelor of Science in Computer
             Science from the Russ College of Engineering and Technology at Ohio
             University.
           </p>
-          <p class="font-mono mb-8 text-lg">🌇 I am from Columbus, Ohio.</p>
-          <p class="font-mono mb-8 text-lg">
+          <p class="text-white font-mono mb-8 text-lg">
+            🌇 I am from Columbus, Ohio.
+          </p>
+          <p class="text-white font-mono mb-8 text-lg">
             🧑‍🔬 I am seeking a full time Software Development position.
           </p>
-          <p class="font-mono mb-1 text-lg"> I have experience in:</p>
-          <p class="font-semibold mb-1 text-lg">
+          <p class="text-white font-mono mb-1 text-lg">I have experience in:</p>
+          <p class="text-white font-semibold mb-1 text-lg">
             ☕ Languages such as Java, Python, SQL, and Javascript.
           </p>
-          <p class="font-semibold mb-1 text-lg">
+          <p class="text-white font-semibold mb-1 text-lg">
             🖼️ Frameworks such as Angular, React, and Spring.
           </p>
-          <p class="font-semibold mb-8 text-lg">
+          <p class="text-white font-semibold mb-8 text-lg">
             💾 Databases such as MySQL and PostgresSQL.
           </p>
-          <p class="font-mono mb-8 text-lg">
+          <p class="text-white font-mono mb-8 text-lg">
             💻 I have a passion for visualizing, designing, and developing
             creative and innovative software solutions to solve real-world
             industry problems.
           </p>
-          <p class="font-mono mb-8 text-lg">
+          <p class="text-white font-mono mb-8 text-lg">
             🔨 I built this website with Next.js (Framework for React) and
             Tailwind CSS.
           </p>
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -5,11 +5,11 @@ export default function ContactComponent() {
     <div class="hero min-h-screen bg-base-200 overflow-auto">
       <div class="text-center hero-content">
         <div class="max-w-md">
-          <h1 class="mb-8 text-6xl font-bold underline">Contact</h1>
+          <h1 class="mb-8 text-white text-6xl font-bold underline">Contact</h1>
           <a href="mailto:rabadiram@gmail.com">
-            <p class="mb-5 text-2xl">Email: rabadiram@gmail.com</p>
+            <p class="mb-5 text-white text-2xl">Email: rabadiram@gmail.com</p>
           </a>
-          <p class="mb-5 text-2xl">Phone: 614-441-7268</p>
+          <p class="mb-5 text-white text-2xl">Phone: 614-441-7268</p>
           <div className="flex justify-center space-x-4">
             <div data-tip="😊" class="tooltip">
               <a
