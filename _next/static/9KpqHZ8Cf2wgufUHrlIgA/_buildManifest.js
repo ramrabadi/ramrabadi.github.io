7f83d483641b9679764b60b7f9b8a8e1fd9052f2
ramrabadi <rabadiram@gmail.com>
2021-11-10 21:19:00 +0000

    Deploying to gh-pages from @ ramrabadi/ramrabadi.github.io@2b118d6e1ea0a9bd0f05d0f04fc8c1b3e0cb39f1 🚀

--- _next/static/9KpqHZ8Cf2wgufUHrlIgA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/73b73e4521e0b5f1.css","static/chunks/pages/index-d5cc74a43574f0a0.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
