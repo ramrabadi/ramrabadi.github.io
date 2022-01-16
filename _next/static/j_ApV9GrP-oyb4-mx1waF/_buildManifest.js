ce34da6b9f4e3f4a75233415e5f048ab3fa2f0a1
ramrabadi <rabadiram@gmail.com>
2022-01-16 23:35:57 +0000

    Deploying to gh-pages from @ ramrabadi/ramrabadi.github.io@fec67dfa6ed38b804a17e1535cf6e5196b6a694e 🚀

--- _next/static/j_ApV9GrP-oyb4-mx1waF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/73b73e4521e0b5f1.css","static/chunks/pages/index-0cefb99964cf193e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/ojuHQp8ROXSBYj2Q9W-aJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/73b73e4521e0b5f1.css","static/chunks/pages/index-487c721b35708803.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
